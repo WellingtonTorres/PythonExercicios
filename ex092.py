8150cd4cefaf3b5ddd14d36ff3189acf90e8bfbe
WellingtonTorres <wellingtontorresmelo@gmail.com>
2020-02-04 22:27:58 -0300

    Atualizando atividades

--- ex092.py
+++ ex092.py
@@ -4,6 +4,10 @@ dados['nome'] = str(input('Nome: '))
 nasc = int(input('Ano de nacsicmento: '))
 dados['idade'] = datetime.now().year - nasc
 dados['ctps'] = int(input('Carteira de trabalho (0 Não tem): '))
+if dados["ctps"] != 0:
+    dados['contratacao'] = int(input('Ano de contratação: '))
+    dados['salario'] = float(input('Salário: '))
+    dados['aposentadoria'] = (dados["contratacao"] + 50) - nasc
 print('=-=' * 30)
 for k, v in dados.items():
     print(f'- {k} tem valor {v}')
