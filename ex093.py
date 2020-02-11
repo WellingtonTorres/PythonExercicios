f8f3b6506fbe5d0596cd5c1ae9ad431be02a7627
WellingtonTorres <wellingtontorresmelo@gmail.com>
2020-02-10 21:45:30 -0300

    Atualizando atividades

--- ex093.py
+++ ex093.py
@@ -1,18 +1,18 @@
 dados = dict()
-gols = list()
+rounds = list()
 dados['nome'] = str(input('Nome jogador: '))
 n_partidas = int(input('    Quantas partidas jogou?'))
 for i in range(0, n_partidas):
-    gols.append(int(input(f'    Quantos gols na partida {i}? ')))
-dados['gols'] = gols
-dados['total'] = sum(gols)
+    rounds.append(int(input(f'    Quantos gols na partida {i}? ')))
+dados['gols'] = rounds[:] #Ajusteo a cópia da lista
+dados['total'] = sum(rounds)
 print('=' * 40)
 print(dados)
 print('=' * 40)
 for k, v in dados.items():
     print(f'O campo {k} tem o valor {v}')
 print('=' * 40)
-print(f'O Jogador {dados["nome"]} jogou {n_partidas} partidas.')
-for pos, val in enumerate(gols):
+print(f'O Jogador {dados["nome"]} jogou {len(dados["gols"])} partidas.')
+for pos, val in enumerate(rounds):
     print(f'    => Na partida {pos}, fez {val} gol(s).')
-print(f'Foi um total de {sum(gols)} gols.')
+print(f'Foi um total de {dados["total"]} gols.')
