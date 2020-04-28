479ec5fe46761ca092e650d575186f64e7c2532d
WellingtonTorres <wellingtontorresmelo@gmail.com>
2020-04-27 23:02:39 -0300

    Dict, cadastro jogador.

--- ex093.py
+++ ex093.py
@@ -1,2 +1,18 @@
 dados = dict()
+gols = list()
 dados['jogador'] = str(input('Nome do jogador: '))
+num_ = int(input('Numero de partidas que o jogador jogou: '))
+for n in range(0, num_):
+    gols.append(int(input(f'     Qunatos gols na partida {n}? ')))
+dados['gols'] = gols
+dados['total'] = sum(gols)
+print('=-=' * 30)
+print(dados)
+print('=-=' * 30)
+for k, v in dados.items():
+    print(f'O campo {k} tem valor {v}')
+print('=-=' * 30)
+print(f'O jogador {dados["jogador"]} jogou {len(gols)} partidas.')
+for pos, v in enumerate(gols):
+    print(f'    => Na partida {pos}, fez {v} gols.')
+print(f'Foi um total de {dados["total"]} gols.')
