b975bbc884d4a1ace91af97544e40e14bf7ab547
WellingtonTorres <wellingtontorresmelo@gmail.com>
2020-05-11 11:03:24 -0300

    Solução @WT

---
+++ ex101.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+
+def voto(anonacimento):
+    '''
+    :param anonacimento: Informar ano de nascimento para saber sobre o seu direito ao voto
+    :return: Não há return
+    @WellingtonTorres
+    '''
+    global idade
+    idade = datetime.now().year - anonacimento
+    print(f'Com {idade} anos:', end=' ')
+    if idade < 14:
+        print('NEGADO.')
+    elif (14 <= idade <= 17) or (idade > 65):
+        print('VOTO OPCIONAL.')
+    elif 18 <= idade <= 65:
+        print('VOTO OBRIGATÓRIO.')
+
+
+# MAIN
+anonasc = int(input('Em qual ano você nasceu? '))
+voto(anonasc)
